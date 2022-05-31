670acca37c9c4e21d1ec7d7cf3c75075406645ed
mochammadshenna <shenawardana@gmail.com>
2022-05-31 11:46:32 +0700

    The Test using second option to optimaize

--- word.js
+++ word.js
@@ -1,19 +1,15 @@
 function matchWord(str) {
-    let tempObj = {}
-    let tempArr = []
-    str.forEach(word => {
-        tempArr = word.split("")
-  
-        temp = tempArr.sort()
-  
-        if (!tempObj[temp]) tempObj[temp] = []
-        tempObj[temp].push(word)
-  
-    })
+    let obj = {}
     let result = []
-    let keys = Object.keys(tempObj)
-    keys.forEach(key => {
-        result.push(tempObj[key])
+
+    str.forEach(word => {
+        let arr = word.split("").sort()
+        if (!obj[arr]) obj[arr] = []
+        obj[arr].push(word)
+    })
+
+    Object.keys(obj).forEach(key => {
+        result.push(obj[key])
     })
     return result
   }
